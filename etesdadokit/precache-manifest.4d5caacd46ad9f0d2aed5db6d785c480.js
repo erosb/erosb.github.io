326dc6d8e3b77182d4f517e773ccc18c1cc001bb
Bence Erős <ebence88@gmail.com>
2020-03-26 23:28:51 +0100

    init demo etesdadokit

---
+++ etesdadokit/precache-manifest.4d5caacd46ad9f0d2aed5db6d785c480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b1461e69931ee41d9e8b2aff0b5469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67316a09e7bd58a987a7",
+    "url": "/static/css/main.fcbdf5f4.chunk.css"
+  },
+  {
+    "revision": "4e872e9a9e433275e84c",
+    "url": "/static/js/2.dc81dac7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dc81dac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67316a09e7bd58a987a7",
+    "url": "/static/js/main.3f65b19b.chunk.js"
+  },
+  {
+    "revision": "1f1966e9fb1e91b0c1ef",
+    "url": "/static/js/runtime-main.0fccec2b.js"
+  }
+]);
