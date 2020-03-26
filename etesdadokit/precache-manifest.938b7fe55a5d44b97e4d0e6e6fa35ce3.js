7524065d43291a6820c9b8b2eca7bc4d36c159c5
Bence Erős <ebence88@gmail.com>
2020-03-26 23:36:22 +0100

    mossing from prev commit

---
+++ etesdadokit/precache-manifest.938b7fe55a5d44b97e4d0e6e6fa35ce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e474ed4fe0762378a2469c295e2f65",
+    "url": "/etesdadokit/index.html"
+  },
+  {
+    "revision": "dfbcd1ccdef868f99fcc",
+    "url": "/etesdadokit/static/css/main.fcbdf5f4.chunk.css"
+  },
+  {
+    "revision": "4e872e9a9e433275e84c",
+    "url": "/etesdadokit/static/js/2.dc81dac7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/etesdadokit/static/js/2.dc81dac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbcd1ccdef868f99fcc",
+    "url": "/etesdadokit/static/js/main.65aaced1.chunk.js"
+  },
+  {
+    "revision": "e549ccea81a59dbf94ec",
+    "url": "/etesdadokit/static/js/runtime-main.f39d7648.js"
+  }
+]);
